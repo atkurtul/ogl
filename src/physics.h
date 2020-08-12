0bd5c85d4f4d64064fb4dcbb5cf62a26b2c8203a
unknown <atilkurtulmus@gmail.com>
2020-08-12 23:14:31 +0300

    fix

--- src/physics.h
+++ src/physics.h
@@ -1,18 +1,19 @@
 #pragma once
-#include "common.h"
 #include "btBulletDynamicsCommon.h"
+#include "common.h"
 
-struct PhysicsSimulator {
-	btDefaultCollisionConfiguration* config;
-	btCollisionDispatcher* dispatcher;
-	btBroadphaseInterface* cache;
-	btSequentialImpulseConstraintSolver* solver;
-	btDiscreteDynamicsWorld* world;
-	btAlignedObjectArray<btCollisionShape*> shapes;
+struct PhysicsSimulator
+{
+	btDefaultCollisionConfiguration *config;
+	btCollisionDispatcher *dispatcher;
+	btBroadphaseInterface *cache;
+	btSequentialImpulseConstraintSolver *solver;
+	btDiscreteDynamicsWorld *world;
+	btAlignedObjectArray<btCollisionShape *> shapes;
 
 	void init();
 
-	btRigidBody* add_body(glm::vec3 bbMin, glm::vec3 bbMax, glm::vec3 pos, glm::vec3 initial_velocity);
+	btRigidBody *add_body(glm::vec3 bbMin, glm::vec3 bbMax, glm::vec3 pos, glm::vec3 initial_velocity);
 
 	void integrate(float t);
 
--- src/scene.cpp
+++ src/scene.cpp
@@ -1,37 +1,40 @@
-#include "glad/glad.h"
-#include "assimp/Importer.hpp"
 #include "assimp/scene.h"
+#include "assimp/Importer.hpp"
 #include "assimp/postprocess.h"
+#include "glad/glad.h"
 #include "scene.h"
 
-Scene& Scene::import(string path)
+Scene &Scene::import(string path)
 {
     printf("importing scene\n");
     Assimp::Importer imp;
     auto scene = imp.ReadFile(path,
-        aiProcess_Triangulate |
-        aiProcess_FlipUVs |
-        aiProcess_GenSmoothNormals |
-        aiProcess_CalcTangentSpace |
-        aiProcess_LimitBoneWeights);
+                              aiProcess_Triangulate |
+                                  aiProcess_FlipUVs |
+                                  aiProcess_GenSmoothNormals |
+                                  aiProcess_CalcTangentSpace |
+                                  aiProcess_LimitBoneWeights);
 
     if (!scene)
         printf("assimp scene null\n");
-    for (uint i = 0; i < scene->mNumMaterials; ++i) {
-        Material* mat = new Material();
+    for (uint i = 0; i < scene->mNumMaterials; ++i)
+    {
+        Material *mat = new Material();
         mat->import(this, scene->mMaterials[i]);
         materials.push_back(mat);
     }
 
     printf("imported mats\n");
-    for (uint i = 0; i < scene->mNumMeshes; ++i) {
-        Mesh* mesh = new Mesh;
+    for (uint i = 0; i < scene->mNumMeshes; ++i)
+    {
+        Mesh *mesh = new Mesh;
         mesh->import(this, scene->mMeshes[i]);
         meshes.push_back(mesh);
     }
     printf("imported meshes\n");
-    for (uint i = 0; i < scene->mNumAnimations; ++i) {
-        Animation* animation = new Animation;
+    for (uint i = 0; i < scene->mNumAnimations; ++i)
+    {
+        Animation *animation = new Animation;
         animation->import(this, scene->mAnimations[i]);
         animations.push_back(animation);
         active_animation = animation;
@@ -42,15 +45,17 @@ Scene& Scene::import(string path)
     return *this;
 }
 
-void Scene::render() {
+void Scene::render()
+{
     printf("rendering\n");
     Light light;
-    light.create(0.1, 120, 60, glm::vec3(-15, 50, -10), glm::vec3(5, 0, 5));
-    while (win.poll()) {
+    light.create(0.1f, 120.f, 60.f, glm::vec3(-15.f, 50.f, -10.f), glm::vec3(5.f, 0.f, 5.f));
+    while (win.poll())
+    {
         update(win.dt);
         RenderState state;
         //render to depth buffer from the pov of light
-        light.update_position(glm::angleAxis(win.dt * 0.2f, glm::vec3(0, 1, 0)) * light.position);
+        light.update_position(glm::angleAxis(win.dt * 0.2f, glm::vec3(0.f, 1.f, 0.f)) * light.position);
         {
             state.active_shader = &shadow_shader;
             glViewport(0, 0, 4096, 4096);
@@ -84,20 +89,27 @@ void Scene::render() {
     }
 }
 
-void Scene::physics_demo() {
+void Scene::physics_demo()
+{
     string base = PROJECT_BASE_BUILD_DIR;
     import(base + "/cube/plane_cube.obj");
-    for (auto node : root->children) {
-        if (node->name == "Cube") {
-            for (int i = -5; i < 5; i+=2) {
-                for (int j = -5; j < 5; j+=2) {
-                    for (int k = 10; k < 20; k += 2) {
+    for (auto node : root->children)
+    {
+        if (node->name == "Cube")
+        {
+            for (int i = -5; i < 5; i += 2)
+            {
+                for (int j = -5; j < 5; j += 2)
+                {
+                    for (int k = 10; k < 20; k += 2)
+                    {
                         auto mesh = node->meshes[0];
                         auto body = simulator.add_body(mesh->bbMin, mesh->bbMax, glm::vec3(i, k, j), glm::vec3());
                         node->bodies[mesh].push_back(body);
                     }
 
-                    for (int k = 50; k < 60; k += 2) {
+                    for (int k = 50; k < 60; k += 2)
+                    {
                         auto mesh = node->meshes[0];
                         auto body = simulator.add_body(mesh->bbMin, mesh->bbMax, glm::vec3(i, k, j), glm::vec3());
                         node->bodies[mesh].push_back(body);
@@ -105,7 +117,8 @@ void Scene::physics_demo() {
                 }
             }
         }
-        if (node->name == "Plane") {
+        if (node->name == "Plane")
+        {
             auto mesh = node->meshes[0];
             auto body = simulator.add_body(mesh->bbMin, mesh->bbMax, glm::vec3(0, -1, 0), glm::vec3(0.f));
             node->bodies[mesh].push_back(body);
@@ -120,9 +133,10 @@ Scene::Scene() : win(1600, 900, "OGL")
     simulator.init();
     active_animation = 0;
     root = new Node;
-    for (int i = 0; i < 10; ++i) {
-        const float pi = 3.1415926535 * 2;
-        const float angle = i * pi / 8;
+    for (int i = 0; i < 10; ++i)
+    {
+        const float pi = 3.1415926535f * 2.f;
+        const float angle = i * pi / 8.f;
         const float r = 15;
         cameras.push_back(new Camera(60.f, 16.f / 9.f, glm::vec3(cosf(angle) * r, 20, sinf(angle) * r), glm::vec3(0)));
     }
@@ -130,8 +144,8 @@ Scene::Scene() : win(1600, 900, "OGL")
     auto diffuse = new Texture;
     auto normal = new Texture;
 
-    unsigned char diff[4] ={ 255, 255, 255, 255 };
-    unsigned char norm[4] ={ 0, 0, 127, 0 };
+    unsigned char diff[4] = {255, 255, 255, 255};
+    unsigned char norm[4] = {0, 0, 127, 0};
 
     diffuse->from_raw(diff, 1, 1, GL_RGBA);
     normal->from_raw(norm, 1, 1, GL_RGBA);
@@ -168,17 +182,24 @@ Scene::~Scene()
     delete default_mat;
     delete root;
 
-    for (auto t : textures) t.second->free();
-    for (auto m : materials) delete m;
-    for (auto m : meshes) delete m;
-    for (auto c : cameras) delete c;
-    for (auto a : animations) delete a;
+    for (auto t : textures)
+        t.second->free();
+    for (auto m : materials)
+        delete m;
+    for (auto m : meshes)
+        delete m;
+    for (auto c : cameras)
+        delete c;
+    for (auto a : animations)
+        delete a;
 }
 
 void Scene::update(float dt)
 {
-    for (int i = 0; i < 10; ++i) {
-        if (win.get_key(GLFW_KEY_0 + i)) {
+    for (int i = 0; i < 10; ++i)
+    {
+        if (win.get_key(GLFW_KEY_0 + i))
+        {
             active_cam = cameras[i];
             break;
         }
@@ -186,7 +207,8 @@ void Scene::update(float dt)
 
     simulator.integrate(dt);
 
-    if (active_animation) {
+    if (active_animation)
+    {
         glm::mat4 bones[128];
         active_animation->update(dt * 25);
         root->update(this, glm::mat4(1.f), bones);
@@ -197,15 +219,16 @@ void Scene::update(float dt)
     }
 }
 
-Texture* Scene::get_texture(string path, Texture::Slot slot)
+Texture *Scene::get_texture(string path, Texture::Slot slot)
 {
     {
         auto tex = textures.find(path);
         if (tex != textures.end())
             return tex->second;
     }
-    Texture* tex = new Texture{};
-    if (tex->load(path)) {
+    Texture *tex = new Texture{};
+    if (tex->load(path))
+    {
         textures[path] = tex;
         return tex;
     }
